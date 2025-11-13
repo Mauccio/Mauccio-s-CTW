e7801b148226e79a504a3d5187d4f3231686efe2
Mauccio <69328427+Mauccio@users.noreply.github.com>
2025-11-13 10:45:55 -0500

    v1.4-BETA

--- Mauccio's CTW/com/mauccio/ctw/files/ConfigManager.java
+++ Mauccio's CTW/com/mauccio/ctw/files/ConfigManager.java
@@ -3,6 +3,7 @@ package com.mauccio.ctw.files;
 import com.mauccio.ctw.CTW;
 import org.bukkit.configuration.file.*;
 import java.io.*;
+import java.util.List;
 
 public class ConfigManager {
     private final CTW plugin;
@@ -28,9 +29,6 @@ public class ConfigManager {
             this.plugin.reloadConfig();
         }
         this.validateSignText(this.getSignFirstLine(), "signs.first-line-text", "ctw");
-        this.validateSignText(this.getSignFirstLineReplacement(), "signs.first-line-text-replacement", "&1LIBELULA&4CTW");
-        this.validateSignText(this.getTextForInvalidRooms(), "signs.on-invalid-room-replacement", "&4INVALID ROOM");
-        this.validateSignText(this.getTextForInvalidMaps(), "signs.on-invalid-map-replacement", "&4INVALID MAP");
         File defaultMapFile = new File(this.plugin.getDataFolder(), "defaultmap.yml");
         if (!defaultMapFile.exists()) {
             this.plugin.saveResource("defaultmap.yml", false);
@@ -38,7 +36,7 @@ public class ConfigManager {
     }
 
     private void validateSignText(String text, String key, String defaultValue) {
-        if (text.length() < 1 || text.length() > 16) {
+        if (text.isEmpty() || text.length() > 16) {
             this.plugin.getLogger().warning("Config value \"".concat(key).concat("\" is incorrect."));
             this.config.set(key, (Object)defaultValue);
             this.plugin.getLogger().info("Config value \"".concat(key).concat("\" has been changed to \"").concat(defaultValue).concat("\"."));
@@ -47,22 +45,6 @@ public class ConfigManager {
 
     public String getSignFirstLine() {
         return this.config.getString("signs.first-line-text");
-    }
-
-    public String getSignFirstLineReplacement() {
-        return this.config.getString("signs.first-line-text-replacement");
-    }
-
-    public String getTextForInvalidRooms() {
-        return this.config.getString("signs.on-invalid-room-replacement");
-    }
-
-    public String getTextForInvalidMaps() {
-        return this.config.getString("signs.on-invalid-map-replacement");
-    }
-
-    public String getTextForDisabledMaps() {
-        return this.config.getString("signs.on-disabled-map");
     }
 
     public boolean implementSpawnCmd() {
@@ -75,5 +57,33 @@ public class ConfigManager {
 
     public boolean isFallDamage() {
         return this.plugin.getConfig().getBoolean("disable-fall-damage", false);
+    }
+
+    public boolean isKitSQL() {
+        return this.plugin.getConfig().getBoolean("use-sql-for-kits", false);
+    }
+
+    public List<String> getBreakableBlocks() {
+        return plugin.getConfig().getStringList("no-protected-blocks");
+    }
+
+    public List<String> getNoCrafteableItems() {
+        return plugin.getConfig().getStringList("no-crafteable-items");
+    }
+
+    public boolean isSoundsEnabled() {
+        return this.plugin.getConfig().getBoolean("sounds.enabled", true);
+    }
+
+    public boolean isGlobalTablistEnabled() {
+        return this.plugin.getConfig().getBoolean("global-tablist", false);
+    }
+
+    public boolean isLobbyGuardEnabled() {
+        return this.plugin.getConfig().getBoolean("lobby-guard", false);
+    }
+
+    public boolean isKitMenuEnabled() {
+        return this.plugin.getConfig().getBoolean("kit-menu", false);
     }
 }
