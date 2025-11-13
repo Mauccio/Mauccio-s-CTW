174bcb07187842b60b8ef44aa5d40e6b8652a22b
Mauccio <69328427+Mauccio@users.noreply.github.com>
2025-11-13 11:09:22 -0500

    v1.4-BETA

---
+++ Mauccio's CTW/com/mauccio/ctw/utils/PlayerStats.java
@@ -0,0 +1,15 @@
+package com.mauccio.ctw.utils;
+
+public class PlayerStats {
+    public final int score;
+    public final int kills;
+    public final int deaths;
+    public final int wools;
+
+    public PlayerStats(int score, int kills, int deaths, int wools) {
+        this.score = score;
+        this.kills = kills;
+        this.deaths = deaths;
+        this.wools = wools;
+    }
+}
